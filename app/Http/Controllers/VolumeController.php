e58c5d6f2fe79ad29a5ed716bb8ccdaa255e3550
joelferreira14 <joel.sa.fe@hotmail.com>
2019-05-17 09:53:23 +0100

    Post do token a funcionar (mas na função de criar volume)

--- app/Http/Controllers/VolumeController.php
+++ app/Http/Controllers/VolumeController.php
@@ -15,23 +15,25 @@ class VolumeController extends Controller
    public function createVolume(){
     	$client = new \GuzzleHttp\Client();
     	$url = 'http://46.101.65.213/identity/v3/auth/tokens';
-    	$body = '{ "auth": { "identity": { "methods": [ "password" ], "password": { "user": { "name": "demo", "domain": { "name": "Default" }, "password": "devstack" } } }, "scope": { "project": { "id": "6fa6b37e2d1a4e49a0704bb7ee2d9f73" } } } }';
+    	$body = '{ "auth": { "identity": { "methods": [ "password" ], "password": { "user": { "name": "D-D", "domain": { "name": "Default" }, "password": "D-D" } } }, "scope": { "project": { "id": "58293217310f47b69785e31aaaad5987" } } } }';
 
     	$response = $client->request('POST', $url, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
     		'body' => $body
 		]);
 
-		//$token = $response->getHeader('X-Subject-Token')[0];
-		//$token = $response->getResponseHeader('X-Subject-Token');
+		$token = $response->getHeader('X-Subject-Token')[0];
 
-		return $response;
+		return $token;
     }
 
 
     public function listVolumes(){
     	$client = new \GuzzleHttp\Client();
     	$url = 'http://46.101.65.213/volume/v3/58293217310f47b69785e31aaaad5987/volumes/detail';
-    	$token = 'gAAAAABc2vAzxvkaUxnqydhbFClSLFEfdBX_sYwUynoX--LRhc_wSekY4Yf7rLUtjLS13emsGyf1zegZBQZq68iL0VM2g4LUboA1j83PMJipVjTDq_GHH784ZuOkQR6-80gmaCAV6hI9CFGDw1ePj16IQkun4FrAvqsZwnggeYxbIbCJllXFyTo';
+    	$token = 'gAAAAABc3nWW0TbAVTekTt49E2IRl5NLZ_mp95O149VLPW4oABMQW2IrIIjFYJeOD8O9aaZAfo0-LxOSGGMU0PQ-qSJfjPB-AOGZ7J-hWGlX5ywW-3qktFGuLVZ777hp2VhOO_8MakAqad80QF0G2qfO-79YNZcvF0qQlbl12LA8Bh3ogcx5lzo';
 
     	
     	$response = $client->request('GET', $url, [
